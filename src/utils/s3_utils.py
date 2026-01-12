f63b1bc6328260a5b52a88529d44ad8540a07121
Rajath Adiga <adigarajath123@gmail.com>
2026-01-12 21:31:21 +0530

    Add S3 presigned URL generator

---
+++ src/utils/s3_utils.py
@@ -0,0 +1,20 @@
+from src.utils.aws_resources import get_s3_client
+from src.config import BUCKET_NAME
+import uuid
+
+
+def generate_upload_url(content_type: str):
+    s3 = get_s3_client()
+    image_key = f"images/{uuid.uuid4()}"
+
+    url = s3.generate_presigned_url(
+        ClientMethod="put_object",
+        Params={
+            "Bucket": BUCKET_NAME,
+            "Key": image_key,
+            "ContentType": content_type,
+        },
+        ExpiresIn=300,
+    )
+
+    return url, image_key
